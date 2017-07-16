0b1c6ab1a5b0312208696bb5fad4705ab8f24556
lucasjkr <lucas@lucaskrupinski.com>
2017-07-16 08:33:29 -0400

    Output of Words can now be (optionally) leetified

--- README.md
+++ README.md
@@ -35,3 +35,20 @@ This is basically a [Correct Horse Battery Stapler](https://www.xkcd.com/936/) g
     echo $password->generate(4);
     
 **Example output:** `groove blare gob treadmill`
+
+
+###Words/Leetify
+Additionally, you can specify "Leet Speak" transformations to be made on the output, and choose the frequency that those subsitutions will occur:
+
+    require 'src/PasswordMakerWords.php';
+    $password  = new Lucasjkr\PasswordMaker\Words();
+    $password->dictionary = "dictionary.txt";
+    $password->leetify    = true;
+    $password->chance     = 10;
+    echo $password->generate(4);
+
+**Example output:** `gro0ve blare gob treadm!ll
+
+I would caution against setting the chance too high - part of the appeal of using a series of words as a password is that its easily memorizable, yet potentially harder to brute force. If you set it too high, it could be harder to memorize, and could become more predicable (as the point is to introduce uncertainty - if an attacker knows that every instance of `e` will be replaced by `3`, you've done nothing but make your password harder to memorize)`
+
+Setting `chance` to 100 will transform output like `compiled braket between dime` into `c0mp!13d 8r4ck37 837w33n d!m3`
--- src/PasswordMakerWords.php
+++ src/PasswordMakerWords.php
@@ -5,6 +5,8 @@ namespace Lucasjkr\PasswordMaker;
 class Words
 {
     private $dictionary;
+    public $leetify = false;
+    public $chance   = 100;
 
     public function __get($property) {
         if (property_exists($this, $property)) {
@@ -25,7 +27,12 @@ class Words
         for ($i = 1; $i <= $number_of_words; $i++) {
             $result .= $this->randomWord() . " ";
         }
-        return trim($result);
+
+        if($this->leetify == true){
+            return $this->leetChars($result);
+        }
+
+        return $result;
     }
 
     private function randomWord() {
@@ -34,8 +41,27 @@ class Words
         $contents = fread($handle, filesize($filename));
         fclose($handle);
         $words = explode("\r", $contents);
-        $result = strtolower($words[rand(0, count($words)-1)]);
+        $result = strtolower($words[random_int(0, count($words)-1)]);
         return $result;
     }
 
+    private function leetChars($string){
+        $chars  = str_split($string);
+        $result = '';
+
+        foreach ($chars as $c) {
+            $r       = random_int(0, 100);
+            $input   = array('a', 'b', 'e', 'g', 'i', 'l', 'o', 's', 't');
+            $output  = array('4', '8', '3', '6', '!', '1', '0', '5', '7');
+
+            if($r <= $this->chance) {
+                $result .= str_replace($input, $output, $c);
+            } else {
+                $result .= $c;
+            }
+
+        }
+
+        return $result;
+    }
 }
